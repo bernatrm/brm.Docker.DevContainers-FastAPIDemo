7f333f15735be1b7296468a64de1c69688b9364a
Bernat Real <bernat.real@hotelsviva.com>
2024-12-29 21:57:08 +0100

    first commit

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+import redis
+
+app = FastAPI()
+
+r = redis.Redis(host="redis", port=6379)
+
+import debugpy
+debugpy.listen(("0.0.0.0", 5678))
+print("Debugger is active. Attach VS Code to port 5678 to debug.")
+# If we want to wait for a debug session to continue or we can add a breakpoint
+# debugpy.wait_for_client()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/hits")
+def read_root():
+    r.incr("hits")
+    return {"number of hits": r.get("hits")}
